123a7d099ba169a671b53432989357dfab9b6b91
nihat23 <62906442+nihat23@users.noreply.github.com>
2021-05-05 11:08:14 +0300

    Sayıyı Yakala

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Dinamik_nerne_ekleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4Kirmizi = new System.Windows.Forms.Label();
+            this.label5Mavi = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1Basla = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(528, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 80);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1450;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(510, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kazanan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Script", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label3.Location = new System.Drawing.Point(505, 404);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 34);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kaybeden";
+            // 
+            // label4Kirmizi
+            // 
+            this.label4Kirmizi.AutoSize = true;
+            this.label4Kirmizi.Font = new System.Drawing.Font("Lucida Handwriting", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4Kirmizi.ForeColor = System.Drawing.Color.Red;
+            this.label4Kirmizi.Location = new System.Drawing.Point(525, 332);
+            this.label4Kirmizi.Name = "label4Kirmizi";
+            this.label4Kirmizi.Size = new System.Drawing.Size(46, 48);
+            this.label4Kirmizi.TabIndex = 3;
+            this.label4Kirmizi.Text = "0";
+            // 
+            // label5Mavi
+            // 
+            this.label5Mavi.AutoSize = true;
+            this.label5Mavi.Font = new System.Drawing.Font("Lucida Handwriting", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5Mavi.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label5Mavi.Location = new System.Drawing.Point(525, 438);
+            this.label5Mavi.Name = "label5Mavi";
+            this.label5Mavi.Size = new System.Drawing.Size(46, 48);
+            this.label5Mavi.TabIndex = 4;
+            this.label5Mavi.Text = "0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(511, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 29);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Script", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(536, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 34);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Skor";
+            // 
+            // button1Basla
+            // 
+            this.button1Basla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1Basla.Location = new System.Drawing.Point(530, 190);
+            this.button1Basla.Name = "button1Basla";
+            this.button1Basla.Size = new System.Drawing.Size(94, 39);
+            this.button1Basla.TabIndex = 7;
+            this.button1Basla.Text = "Başla";
+            this.button1Basla.UseVisualStyleBackColor = true;
+            this.button1Basla.Click += new System.EventHandler(this.button1Basla_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(3, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(489, 490);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(660, 514);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1Basla);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5Mavi);
+            this.Controls.Add(this.label4Kirmizi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sayıyı Yakala";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4Kirmizi;
+        private System.Windows.Forms.Label label5Mavi;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1Basla;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dinamik_nerne_ekleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int artipuan = 0, eksipuan = 0;
+
+        void button()
+        {
+            int sayac = 0;
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    sayac++;
+                    Button Btn = new Button();
+                    Btn.Size = new Size(48, 48);
+                    Btn.Location = new Point(j * 48 + 5, i * 48 + 5);
+                    Btn.Name = sayac.ToString();
+                    Btn.ForeColor = Color.Black;
+                    Btn.Text = sayac.ToString();
+                    panel1.Controls.Add(Btn);
+                    Btn.Click += Btn_Click;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Enabled = false;
+            button();
+
+        }
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (int.Parse(btn.Text) == int.Parse(label1.Text))
+            {
+                artipuan++;
+                btn.BackColor = Color.Red;
+                label4Kirmizi.Text = artipuan.ToString();
+            }
+            else
+            {
+                eksipuan++;
+                btn.BackColor = Color.Blue;
+                label5Mavi.Text = eksipuan.ToString();
+            }
+            //MessageBox.Show(((Button)sender).Text + ". Butuna Tıkladınız..", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        Random random = new Random();
+
+        private void button1Basla_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                int sskor = Convert.ToInt32(textBox1.Text);
+
+                if (sskor >= 0)
+                {                   
+                    timer1.Start();
+                    textBox1.ReadOnly = true;
+                    button1Basla.Enabled = false;
+                    panel1.Enabled = true;
+                    artipuan = 0;
+                    eksipuan = 0;
+                    label4Kirmizi.Text = artipuan.ToString();
+                    label5Mavi.Text = eksipuan.ToString();
+                    
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lütfen Sayısal Bir deger giriniz..", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        } 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = random.Next(0, 100).ToString();
+
+            if (int.Parse(textBox1.Text) == int.Parse(label4Kirmizi.Text))
+            {
+                timer1.Stop();
+                button1Basla.Enabled = true;
+                textBox1.ReadOnly = false;
+                artipuan = 0;
+                eksipuan = 0;                
+                panel1.Enabled = false;
+                MessageBox.Show("Tebrikler Kazandınız..", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+
+
+
+            }
+           if (int.Parse(textBox1.Text) == int.Parse(label5Mavi.Text))
+            {
+                timer1.Stop();
+                button1Basla.Enabled = true;
+                textBox1.ReadOnly = false;
+                artipuan = 0;
+                eksipuan = 0;               
+                panel1.Enabled = false;
+                MessageBox.Show("Üzgünüz Kaybettiniz..", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                 
+
+            }
+        }
+    }
+}
